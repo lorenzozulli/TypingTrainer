37f47bb5b8b05d39f0e7c137004c3107b933e8b6
Lorenzo Zulli <zullilorenzo.24@gmail.com>
2024-05-07 11:01:50 +0200

    LogIn Implementato

--- Controller/ControllerAutenticazione.py
+++ Controller/ControllerAutenticazione.py
@@ -3,26 +3,30 @@ import os
 
 from PyQt5 import QtGui, QtWidgets
 from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
 
 from View import LandingPageUtenteView
 class ControllerAutenticazione(object):
 
     #--- metodo per caricare la lista degli utenti ---
     def caricaListaUtenti(self):
-        with open('listaUtenti.json') as f:
+        with open(os.path.join('BaseDiDati', 'listaUtenti.json'), 'r') as f:
             self.listaUtenti = json.load(f)
 
     #--- metodo per effettuare il login ---
-    def logIn(self):
+    def logIn(self, username, password):
         self.caricaListaUtenti()
-        for utente in self.listaUtenti:
-            if self.UsernameInput.text() == utente.utente['username']:
-                if self.PasswordInput.text() == utente.utente['password']:
-                    self.authorized = QtWidgets.QMainWindow()
-                    LandingPageUtenteView().setupUi(self.authorized)
-                    self.authorized.show()
 
-
+        for i in self.listaUtenti['Utenti']:
+            if i['username'] == username:
+                if i['password'] == password:
+                    if i['isAdmin'] == "False":
+                        return "Utente"
+                    else:
+                        return "Admin"
+                else:
+                    return "PasswordErrata"
+        return "UsernameNonTrovato"
         '''
     def logOut():
         #TODO: fare questa funzione

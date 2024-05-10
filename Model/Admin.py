5a62aa98ada6f3649669b0a678007c99c6d8b111
Lorenzo Zulli <zullilorenzo.24@gmail.com>
2024-05-10 09:08:41 +0200

    Sistemati login e registrazione in termini funzionali, ora devo solo mettere le guardie

--- Model/Admin.py
+++ Model/Admin.py
@@ -1,10 +1,11 @@
 import os
 import pickle
 
-from Model.Utilizzatore import Utilizzatore, ListUtilizzatore
+from Model.Utilizzatore import Utilizzatore
 
 class Admin(Utilizzatore):
-     
+            
     def loadAdmin(self):
+        listaUtilizztatori = [self]
         with open(os.path.join('BaseDiDati', 'listaUtilizzatori.pickle'), 'wb') as f:
-            pickle.dump(self, f)
+            pickle.dump(listaUtilizztatori, f)
